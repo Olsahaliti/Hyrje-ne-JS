d82039965f6c37117d8ab1a091f37f458054478d
Olsahaliti <olsahaliti931@gmail.com>
2024-11-18 09:43:04 -0800

    punova

---
+++ script.js
@@ -0,0 +1,16 @@
+//deklarimi i variables = krijojme nje variabel
+let testVar;
+//deklarimi - variables i japim vlere per here te pare
+testVar ="m"
+//deklarimi dhe incializimi ne te njejtin rresht
+const PI = 3,14;
+
+let emri="Olsa"//string
+let notaMatematike=1;//number
+let isAdult=false;//true//boolean
+let x//undefined
+let nullVariable=null
+
+//case sensitive
+
+
