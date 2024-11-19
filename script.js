39bfdd925c6674cd29a068c531d1ebde624399ee
Olsahaliti <olsahaliti931@gmail.com>
2024-11-19 09:48:07 -0800

    vazhdova pune

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let testVar;
 //deklarimi - variables i japim vlere per here te pare
 testVar ="m"
 //deklarimi dhe incializimi ne te njejtin rresht
-const PI = 3,14;
+const PI = 3.14;
 
 let emri="Olsa"//string
 let notaMatematike=1;//number
---
+++ script2.js
@@ -0,0 +1,38 @@
+//alert("shfaq nje mesazh");
+
+//let eshteMbi7 = confirm("A i ki mbi 7 vjet? ")
+//console.log(eshteMbi7)
+
+//let emri = prompt("shkruaj emrin");
+//alert("Hello" + emri);
+
+//plus
+//let nr1 = 4;
+//let nr2 = 3;
+
+//let shuma = nr1 + nr2;//7
+//let ndryshimi = nr1 - nr2;//1
+//let rezultatiiShumezimit = nr1 * nr2;//12
+//let rezultatiiPjesetimit = nr1 / nr2;
+
+//console.log(
+  //  "Shuma e" + nr1 + "dhe" + nr2 + "eshte" + shuma
+//)
+//console.log(
+  //  "Zbritja e" + nr1 + "dhe" + nr2 + "eshte" + ndryshimi
+//)
+//console.log(
+  //  "Shumezimi mes" + nr1 + "dhe" + nr2 + "eshte" + rezultatiiShumezimit
+//)
+//console.log(
+  //  "Pjesetimi mes" + nr1 + "dhe" + nr2 + "eshte" + rezultatiiPjesetimit
+//)
+
+//let ngritjaNeFuqi = 4 ** 2;
+
+//let modulo = 16 % 3;
+
+let numri1 = +prompt("Shkruaj numrin e pare")
+let numri2 = +prompt("Shkruaj numrin e dyte")
+
+console.log(numri1 + numri2);
